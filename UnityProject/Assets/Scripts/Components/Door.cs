a0180ff513aadd3c87057adc11e0cc795db8a1c3
Devin Aldrich <19yoshi95@gmail.com>
2015-04-17 11:35:39 -0400

    Reintegrated my branch into the new repo
    Signed-off-by: Devin Aldrich <19yoshi95@gmail.com>

--- UnityProject/Assets/Scripts/Components/Door.cs
+++ UnityProject/Assets/Scripts/Components/Door.cs
@@ -1,33 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Door : MonoBehaviour
-{
-    public List<GameObject> requisites;
+public class DoorComponent : MonoBehaviour {
 
-    // Use this for initialization
-    private void Start()
-    {
-        requisites = new List<GameObject>();
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Player" /*|| collision.gameObject.GetComponent<Arrow>.getOwner().gameobject.tag == "Player"*/)
-        {
-            if (requisites.Count == 0)
-                openDoor();
-        }
-    }
-
-    public void openDoor()
-    {
-        Destroy(gameObject);
-    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 }
--- UnityProject/Assets/Scripts/Managers/GameManager.cs
+++ UnityProject/Assets/Scripts/Managers/GameManager.cs
@@ -1,65 +1,15 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class GameManager : MonoBehaviour
-{
-    private float musicVol = 100.0f, fxVol = 100.0f;
-    public AudioSource music;
-    private int currentLevel = 0;
+public class GameManager : MonoBehaviour {
 
-    private void Start()
-    {
-        GameObject.DontDestroyOnLoad(gameObject);
-    }
-
-    public void LoadNextLevel()
-    {
-        if (currentLevel < Application.levelCount)
-            currentLevel++;
-        Application.LoadLevel(currentLevel);
-    }
-
-    // Update is called once per frame
-    private void UpdateMusic()
-    {
-        if (music != null)
-            music.volume = musicVol / 100;
-        else
-            Debug.LogWarning("Trying to adjust music volume with no AudioSource attached.");
-    }
-
-    private void UpdateFx()
-    {
-        AudioListener.volume = fxVol / 100;
-    }
-
-    public void musicVolUp()
-    {
-        musicVol += 5.0f;
-        if (musicVol > 100.0f)
-            musicVol = 100.0f;
-        UpdateMusic();
-    }
-
-    public void musicVolDown()
-    {
-        musicVol -= 5.0f;
-        if (musicVol < 0)
-            musicVol = 0;
-        UpdateMusic();
-    }
-
-    public void fxVolUp()
-    {
-        fxVol += 5.0f;
-        if (fxVol > 100.0f)
-            fxVol = 100.0f;
-    }
-
-    public void fxVolDown()
-    {
-        fxVol -= 5.0f;
-        if (fxVol < 0)
-            fxVol = 0;
-    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 }
