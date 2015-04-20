7d31d67dff01dea8bbb50e3c8d938bcb42972681
Devin Aldrich <19yoshi95@gmail.com>
2015-04-20 19:24:45 -0400

    Crumbling Platform LOOSELY sketched. -Platform falls after X amount of time the player has stood on it -Platform currently emits particles indicating time left
    questions:
    -Should the platforms continue to crumble even after the player has gotten off of them?
    
    Signed-off-by: Devin Aldrich <19yoshi95@gmail.com>

---
+++ UnityProject/Assets/Scripts/Level/CrumblingPlatformBehavior.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrumblingPlatformBehavior : MonoBehaviour {
+
+	private float lifetime;
+	public float lifetimeMax = 2f;
+	private Vector2 vibdir;
+
+	bool deathrow = false;
+
+	// Use this for initialization
+	void Start () {
+		lifetime = lifetimeMax;
+		vibdir = new Vector2 (1,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer> ();
+		ParticleSystem psys = gameObject.GetComponent<ParticleSystem> ();
+
+		renderer.color = new Color (renderer.color.r, renderer.color.g * (lifetime/lifetimeMax), renderer.color.b * (lifetime/lifetimeMax));
+
+		if (!deathrow) {
+
+			if (lifetime > lifetimeMax / 7) {
+				vibdir = vibdir.normalized * (lifetimeMax / lifetime) * 3;
+				psys.emissionRate = 1 * (lifetimeMax / lifetime);
+			}
+
+			if (lifetime < lifetimeMax) {
+				rigidbody2D.velocity = vibdir;
+				vibdir *= -1;
+			}
+
+			if (lifetime <= 0.0f) {
+				psys.startSpeed = 0;
+				rigidbody2D.velocity = new Vector2 (0, 0);
+				rigidbody2D.isKinematic = false;
+				rigidbody2D.gravityScale = 1;
+				deathrow = true;
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (rigidbody2D.isKinematic == false)
+			Destroy (this.gameObject);
+	}
+
+	void OnCollisionStay2D(Collision2D coll){
+		if (coll.gameObject.tag == "Player") {
+			lifetime -= Time.deltaTime;
+		}
+	}
+}
