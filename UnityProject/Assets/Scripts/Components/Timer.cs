fcfc341cedd57aab9180ffad6e407de54d37983b
Garrett <gldavis@fullsail.edu>
2015-04-20 11:02:03 -0400

    Added Game Over Canvas. Added timer script.

---
+++ UnityProject/Assets/Scripts/Components/Timer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+    public float waitTime;
+
+	// Update is called once per frame
+	void Update () {
+        waitTime -= Time.deltaTime;
+	}
+}
