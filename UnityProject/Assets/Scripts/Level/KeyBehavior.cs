975584a86288e13ed56bb1f429b9d2e335e4f87b
Devin Aldrich <19yoshi95@gmail.com>
2015-04-20 20:45:27 -0400

    Key behaviour roughly sketched.
    Signed-off-by: Devin Aldrich <19yoshi95@gmail.com>

---
+++ UnityProject/Assets/Scripts/Level/KeyBehavior.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyBehavior : MonoBehaviour {
+
+	ParticleSystem psys;
+
+	bool touched = false;
+
+	float dieTimer;
+	public float dieTimerMax = 5f;
+
+	// Use this for initialization
+	void Start () {
+		psys = gameObject.GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (touched) {
+			dieTimer -= Time.deltaTime;
+			SpriteRenderer sprndr = gameObject.GetComponent<SpriteRenderer>();
+			sprndr.color = new Color(sprndr.color.r,sprndr.color.g,sprndr.color.b,(dieTimer/dieTimerMax));
+
+			if(dieTimer <= 0.0f)
+				Destroy(gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Player") {
+			psys.Emit (100);
+			dieTimer = dieTimerMax;
+			touched = true;
+		}
+	}
+}
