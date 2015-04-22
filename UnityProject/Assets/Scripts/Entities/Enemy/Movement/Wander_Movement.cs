0ae044afda596f7ec7acbc6b36951b88c647e967
Devin Aldrich <19yoshi95@gmail.com>
2015-04-21 22:03:29 -0400

    Enemy Movement Testing Scene has been added!
    Wander_Movement script has also been roughly sketched out.
    
    Signed-off-by: Devin Aldrich <19yoshi95@gmail.com>

--- UnityProject/Assets/Scripts/Entities/Enemy/Movement/Wander_Movement.cs
+++ UnityProject/Assets/Scripts/Entities/Enemy/Movement/Wander_Movement.cs
@@ -3,13 +3,54 @@ using System.Collections;
 
 public class Wander_Movement : MonoBehaviour {
 
+	public bool direction;
+	public float moveSpeed;
+
+	private Vector3 preserveUp;
+
+	bool grounded;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		RandomizeDirection ();
+
+		preserveUp = this.transform.up;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (grounded) {
+			Vector2 movevec = new Vector2(moveSpeed,0);
+
+			if(!direction)
+				movevec *= -1;
+
+			rigidbody2D.velocity = movevec;
+		}
+
+		this.transform.up = preserveUp;
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Platform" && !grounded) {
+			grounded = true;
+			RandomizeDirection();
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D coll){
+		if (coll.gameObject.tag == "Platform") {
+			grounded = false;
+				rigidbody2D.velocity = Vector2.zero;
+		}
+	}
+
+	void RandomizeDirection(){
+		if (Random.Range (1, 6) > 3)
+			direction = true; //right
+		else
+			direction = false; //left
 	}
 }
--- UnityProject/Assets/Scripts/Level/KeyBehavior.cs
+++ UnityProject/Assets/Scripts/Level/KeyBehavior.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class KeyBehavior : MonoBehaviour {
 
+	public GameObject attachedDoor;
+
 	ParticleSystem psys;
+	ParticleSystem.Particle[] finalparticles;
+	int numparticles;
 
 	bool touched = false;
 
@@ -28,11 +32,59 @@ public class KeyBehavior : MonoBehaviour {
 		}
 	}
 
+	void LateUpdate(){
+		if (touched) {
+			numparticles = psys.GetParticles(finalparticles);
+			
+			if(dieTimer <= 2* dieTimerMax/3){
+				for(int i = 0; i < numparticles; ++i){
+					Vector3 to =
+						(attachedDoor.transform.position - finalparticles[i].position).normalized * (dieTimerMax/dieTimer);
+
+					finalparticles[i].velocity =
+						new Vector3(finalparticles[i].velocity.x + to.x,
+						            finalparticles[i].velocity.y + to.y,
+						            psys.transform.position.z);
+				}
+			}else{
+				for (int i = 0; i < numparticles; ++i) {
+					finalparticles [i].velocity =
+						new Vector3(Mathf.SmoothStep(finalparticles[i].velocity.x,0,Time.deltaTime),
+						            Mathf.SmoothStep(finalparticles[i].velocity.y,0,Time.deltaTime),
+						            psys.transform.position.z);
+				}
+			}
+
+			psys.SetParticles(finalparticles,numparticles);
+			
+		}
+	}
+
 	void OnCollisionEnter2D(Collision2D coll){
-		if (coll.gameObject.tag == "Player") {
+		if (coll.gameObject.tag == "Player" && !touched) {
+
+			Destroy (gameObject.collider2D);
+			Destroy (gameObject.rigidbody2D);
+
+			psys.startSpeed = 10;
 			psys.Emit (100);
+			psys.simulationSpace = ParticleSystemSimulationSpace.World;
+
 			dieTimer = dieTimerMax;
 			touched = true;
+
+			finalparticles = new ParticleSystem.Particle[psys.maxParticles];
+
+			numparticles = psys.GetParticles (finalparticles);
+
+			for (int i = 0; i < numparticles; ++i) {
+
+				int modif = Random.Range (10, 20);
+
+				finalparticles [i].velocity = 
+						new Vector3 (finalparticles [i].velocity.x * modif, finalparticles [i].velocity.y * modif, finalparticles [i].velocity.z);
+			}
+			psys.SetParticles (finalparticles, numparticles);
 		}
 	}
 }
