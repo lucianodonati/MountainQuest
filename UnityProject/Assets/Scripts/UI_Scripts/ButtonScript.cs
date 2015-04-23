c6a994827e010535831f7a3525c59cf3e7663329
Luciano Donati <luciano.donati536@gmail.com>
2015-04-23 10:59:00 -0400

    Added button script (read below for details), menus managed by game manager, and more stuff.
    The button script has 2 functions: Load and SwitchToMenu. If you use Load, you select a SCENE from the scene dropdown; otherwise you select the switchToMenu function and a MENU from the menu dropdown. When you choose the function, the non-related dropdown is ignored. Enjoy !

---
+++ UnityProject/Assets/Scripts/UI_Scripts/ButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameManager.Menus _menu = 0;
+    public GameManager.Scenes _scene = 0;
+
+    public void SwitchMenu()
+    {
+        GameManager.instance.switchToMenu(_menu);
+    }
+
+    public void LoadScene()
+    {
+        GameManager.instance.Load(_scene);
+    }
+}
