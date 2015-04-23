5c2f470aa9d908ed667ec095a6e03b99c8f6145b
Devin Aldrich <19yoshi95@gmail.com>
2015-04-23 19:50:48 -0400

    PlayerController Updated! -Looking code abstracted to Look() for easier use and location for animation 	-Parameter and Case added to Look() for possible cutscene use. 	 (When public gameobject looktarget is null, it acts as normal 	 when it's set to something, the player turns to face it). -Walking code abstracted to Walk() for easier use and location for animation -Jumping code abstracted to Jump() for easier use and location for animation -Weapon Switching code abstracted to SwitchWeapon() -Attack code abstracted to Attack()
    Signed-off-by: Devin Aldrich <19yoshi95@gmail.com>

--- UnityProject/Assets/Scripts/Entities/Player/PlayerController.cs
+++ UnityProject/Assets/Scripts/Entities/Player/PlayerController.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// MOVEMENT VARS
-	//calculated for rigidbody2d
+	// calculated for rigidbody2d
 	// mass 5
 	// linear drag 0
 	// angular drag 0.05
 	// gravity scale 4
 
+	// MOVEMENT VARS
 	public float movementSpeed = 10;
 
 	public float jumpSpeed = 1f;
@@ -43,6 +43,9 @@ public class PlayerController : MonoBehaviour {
 	//SWITCH BOOL
 	bool usingSword = false;
 
+	//target for looking
+	public Vector3 looktarget;
+
 	// Use this for initialization
 	void Start () {
 		preserveUp = this.transform.up;
@@ -56,51 +59,16 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 
 		//MOVEMENT
-		if (!swinging) {
-			if (Camera.main.ScreenToWorldPoint (Input.mousePosition).x > transform.position.x)
-				facingRight = true;
-			else if (Camera.main.ScreenToWorldPoint (Input.mousePosition).x < transform.position.x)
-				facingRight = false;
-		}
+		Look (looktarget);
 
-		if (usingSword) {
-			if (facingRight) {
-				transform.GetChild (0).position = Vector3.Lerp (transform.GetChild (0).position,
-			                                              transform.GetChild (0).parent.position + new Vector3 (0.25f, 0, -0.5f),
-			                                              (8 * Time.deltaTime));
-			} else {
-				transform.GetChild (0).position = Vector3.Lerp (transform.GetChild (0).position,
-			                                              transform.GetChild (0).parent.position + new Vector3 (-0.25f, 0, -0.5f),
-			                                              (8 * Time.deltaTime));
-			}
-		} else {
-			Vector3 mousepos = GameObject.FindGameObjectWithTag ("MainCamera").camera.ScreenToWorldPoint (Input.mousePosition);
-			mousepos -= transform.position;
-			mousepos.z = -0.5f;
-
-			transform.GetChild(0).position =
-				Vector3.Lerp(transform.GetChild(0).position,
-				             transform.GetChild (0).parent.position + (mousepos.normalized * 0.25f),
-				             (8*Time.deltaTime));
-		}
-
-		float xMove = Input.GetAxisRaw ("Horizontal");
-		
-		this.gameObject.rigidbody2D.velocity = new Vector2 (xMove * movementSpeed, this.gameObject.rigidbody2D.velocity.y);
+		Walk ();
 
 		if (grounded) {
 			jumpCooldownTimer -= Time.deltaTime;
 		}
 
 		if (Input.GetAxisRaw ("Vertical") > 0 && jumpCooldownTimer <= 0.0f && jumpTimer > 0.0f && !jumplock) {
-			if(jumpTimer == jumpTimerMax && grounded){
-				rigidbody2D.velocity = new Vector2(this.gameObject.rigidbody2D.velocity.x,jumpSpeed * firstJumpModifier);
-			}else if(jumpTimer < jumpTimerMax/5){
-				rigidbody2D.velocity = new Vector2(this.gameObject.rigidbody2D.velocity.x, rigidbody2D.velocity.y + jumpSpeed * firstJumpModifier/3);
-				jumplock = true;
-			}
-
-			jumpTimer-=Time.deltaTime;
+			Jump ();
 		}
 
 		if(Input.GetAxisRaw("Vertical") == 0 && !grounded && !jumplock){
@@ -111,38 +79,118 @@ public class PlayerController : MonoBehaviour {
 
 		if(Input.GetKeyDown(KeyCode.Space)){
 			usingSword = !usingSword;
-
-			if(usingSword){
-				GameObject sord = (GameObject)Instantiate(Sword,transform.position,Quaternion.Euler(0,0,-45));
-				sord.transform.parent = transform;
-				sord.transform.position = sord.transform.parent.position + new Vector3(0.5f,0.5f,-1f);
-			}else{
-				Destroy(transform.GetChild(1).gameObject);
-			}
+			SwitchWeapon();
 		}
 
+		Attack ();
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Platform") {
+			grounded = true;
+			jumpCooldownTimer = jumpCooldownTimerMax;
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D coll){
+		if (coll.gameObject.tag == "Platform") {
+			grounded = true;
+			jumplock = false;
+			jumpTimer = jumpTimerMax;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D coll){
+		grounded = false;
+	}
+
+	void Look(Vector3 lookat){
+
+		Vector3 targpos;
+
+		if (lookat == Vector3.zero)
+			targpos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		else
+			targpos = lookat;
+
+		if (!swinging) {
+			if (targpos.x > transform.position.x)
+				facingRight = true;
+			else if (targpos.x < transform.position.x)
+				facingRight = false;
+		}
+
+		if (usingSword) {
+			if (facingRight) {
+				transform.GetChild (0).position = Vector3.Lerp (transform.GetChild (0).position,
+				                                                transform.GetChild (0).parent.position + new Vector3 (0.25f, 0, -0.5f),
+				                                                (8 * Time.deltaTime));
+			} else {
+				transform.GetChild (0).position = Vector3.Lerp (transform.GetChild (0).position,
+				                                                transform.GetChild (0).parent.position + new Vector3 (-0.25f, 0, -0.5f),
+				                                                (8 * Time.deltaTime));
+			}
+		} else {
+			targpos -= transform.position;
+			targpos.z = -0.5f;
+
+			transform.GetChild (0).position =
+				Vector3.Lerp (transform.GetChild (0).position,
+				              transform.GetChild (0).parent.position + (targpos.normalized * 0.25f),
+				             (8 * Time.deltaTime));
+		}
+		
+	}
+
+	void Walk(){
+		rigidbody2D.velocity = new Vector2 (Input.GetAxisRaw ("Horizontal") * movementSpeed, this.gameObject.rigidbody2D.velocity.y);
+	}
+
+	void Jump(){
+		if(jumpTimer == jumpTimerMax && grounded){
+			rigidbody2D.velocity = new Vector2(this.gameObject.rigidbody2D.velocity.x,jumpSpeed * firstJumpModifier);
+		}else if(jumpTimer < jumpTimerMax/5){
+			rigidbody2D.velocity = new Vector2(this.gameObject.rigidbody2D.velocity.x, rigidbody2D.velocity.y + jumpSpeed * firstJumpModifier/3);
+			jumplock = true;
+		}
+		
+		jumpTimer-=Time.deltaTime;
+	}
+
+	void SwitchWeapon(){
+		if(usingSword){
+			GameObject sord = (GameObject)Instantiate(Sword,transform.position,Quaternion.Euler(0,0,-45));
+			sord.transform.parent = transform;
+			sord.transform.position = sord.transform.parent.position + new Vector3(0.5f,0.5f,-1f);
+		}else{
+			Destroy(transform.GetChild(1).gameObject);
+		}
+	}
+
+	void Attack(){
 		if (!usingSword) {
+
 			//FIRING ARROWS
 			if (arrowCooldownTimer > 0.0f)
 				arrowCooldownTimer -= Time.deltaTime;
-
+			
 			if (Input.GetMouseButton (0) && arrowCooldownTimer <= 0.0f) {
-
+				
 				Vector3 mousepos = GameObject.FindGameObjectWithTag ("MainCamera").camera.ScreenToWorldPoint (Input.mousePosition);
 				mousepos -= transform.position;
 				mousepos.z = 0;
-
+				
 				GameObject currArrow = (GameObject)Instantiate (Arrow,
 				                                                gameObject.transform.position + Vector3.back,
 				                                                Quaternion.FromToRotation (preserveUp, mousepos));
-
+				
 				currArrow.rigidbody2D.velocity = mousepos.normalized * 7.5f;
-
+				
 				arrowCooldownTimer = arrowCooldownTimerMax;
 			}
 		} else {
-			//SWORD CODE
 
+			//SWORD CODE
 			if(!swinging){
 				if(!facingRight && transform.GetChild(1).rotation != Quaternion.Euler(0,0,45)){
 					transform.GetChild(1).rotation = Quaternion.Slerp(transform.GetChild(1).rotation,
@@ -186,14 +234,14 @@ public class PlayerController : MonoBehaviour {
 						
 					}
 				}
-
+				
 				transform.GetChild(1).rotation = Quaternion.Slerp(transform.GetChild(1).rotation,
 				                                                  toRot,
 				                                                  (16*Time.deltaTime));
 				transform.GetChild(1).position = Vector3.Lerp(transform.GetChild(1).position,
 				                                              transform.GetChild(1).parent.position + toPos,
 				                                              (16*Time.deltaTime));
-
+				
 				if(Quaternion.Angle(transform.GetChild(1).rotation,toRot) < 0.1f && !halfswung){
 					halfswung = true;
 				}else if (Quaternion.Angle(transform.GetChild(1).rotation,toRot) < 0.1f && halfswung){
@@ -202,24 +250,5 @@ public class PlayerController : MonoBehaviour {
 				}
 			}
 		}
-	}
-
-	void OnCollisionEnter2D(Collision2D coll){
-		if (coll.gameObject.tag == "Platform") {
-			grounded = true;
-			jumpCooldownTimer = jumpCooldownTimerMax;
-		}
-	}
-
-	void OnCollisionStay2D(Collision2D coll){
-		if (coll.gameObject.tag == "Platform") {
-			grounded = true;
-			jumplock = false;
-			jumpTimer = jumpTimerMax;
-		}
-	}
-
-	void OnCollisionExit2D(Collision2D coll){
-		grounded = false;
 	}
 }
