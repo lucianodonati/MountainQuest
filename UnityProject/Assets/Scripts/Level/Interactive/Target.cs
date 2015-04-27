1b91ab23aa59328f2f14da208f676096278fba80
Garrett <gldavis@fullsail.edu>
2015-04-27 11:41:39 -0400

    Fixed Target script and prefab to allow arrows to attach then get destroyed with Target.

--- UnityProject/Assets/Scripts/Level/Interactive/Target.cs
+++ UnityProject/Assets/Scripts/Level/Interactive/Target.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Arrow")
             Destroy(this.gameObject);
