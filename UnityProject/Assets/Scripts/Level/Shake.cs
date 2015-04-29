26e739d2a53dfb3435622ec081170403308e6694
Luciano Donati <luciano.donati536@gmail.com>
2015-04-29 01:46:43 -0400

    EARTHQUAKE MOTHAFACKAAAAAA

---
+++ UnityProject/Assets/Scripts/Level/Shake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public float duration = 2.0f;
+    private Vector3 originalPosition;
+
+    // Use this for initialization
+    private void Start()
+    {
+        originalPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        gameObject.transform.position += new Vector3(Random.Range(-0.06f, 0.06f), Random.Range(-0.06f, 0.06f), 0);
+        duration -= Time.deltaTime;
+
+        if (duration <= 0.0f)
+            Destroy(this);
+    }
+
+    private void OnDestroy()
+    {
+        transform.position = originalPosition;
+    }
+}
