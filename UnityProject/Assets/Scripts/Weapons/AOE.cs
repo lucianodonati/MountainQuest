55dfaa14186e7e30fc216928de22f4b0de734d14
Garrett <gldavis@fullsail.edu>
2015-04-29 07:30:27 -0400

    Forgot AOE script.

---
+++ UnityProject/Assets/Scripts/Weapons/AOE.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AOE : MonoBehaviour {
+    public float duration;
+    public float maxRadius;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<CircleCollider2D>().radius = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CircleCollider2D circle = GetComponent<CircleCollider2D>();
+        if (circle.radius < maxRadius)
+            circle.radius += maxRadius / duration * Time.deltaTime;
+        else
+        {
+            circle.enabled = false;
+            enabled = false;
+        }
+	}
+}
