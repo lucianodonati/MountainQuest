048e209d2b828d0e358a6c0c194fe99a10b04603
Luciano Donati <luciano.donati536@gmail.com>
2015-05-04 18:22:04 -0400

    asd

---
+++ UnityProject/Assets/Scripts/Level/Interactive/Target (1).cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Arrow")
+            Destroy(this.gameObject);
+    }
+}
