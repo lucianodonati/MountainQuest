71e9c3b2fb46a85b58650c5b560a67c8a4a231d9
Luciano Donati <luciano.donati536@gmail.com>
2015-05-07 18:11:15 -0400

    LoadingScreen Done

---
+++ UnityProject/Assets/Scripts/Level/LoadingScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadingScreen : MonoBehaviour
+{
+    private bool loading = true;
+    public float minLoadingTime = 0;
+
+    private void Awake()
+    {
+        //Application.LoadLevel((int)GameManager.instance.currentLevel);
+    }
+
+    private void Update()
+    {
+        if (!this.gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("LoadingAnimation"))
+        {
+            GameManager.instance.FinishLoading();
+        }
+    }
+}
--- UnityProject/Assets/Scripts/Managers/GameManager.cs
+++ UnityProject/Assets/Scripts/Managers/GameManager.cs
@@ -52,7 +52,7 @@ public class GameManager : MonoBehaviour
         MainMenu, Tutorial, Level1, Level2, Level3, Level4, Level5
     };
 
-    private Scenes currentLevel = Scenes.MainMenu;
+    public Scenes currentLevel = Scenes.MainMenu;
 
     #endregion Scenes
 
@@ -209,44 +209,47 @@ public class GameManager : MonoBehaviour
 
     private void OnLevelWasLoaded(int level)
     {
-        MenuInstances.Clear();
-        for (int i = 0; i < MenuPrefabsDONOTTOUCH.Count + 1; i++)
-            MenuInstances.Add(null);
-        transform.parent = Camera.main.transform;
-        UpdateMusic(musicVol);
-        UpdateSFx(sfxVol);
-        pause = false;
-        music.pitch = 1.0f;
-        Time.timeScale = 1.0f;
-        if (currentLevel != Scenes.MainMenu)
+        if (Application.loadedLevelName != "LoadingScreen")
         {
-            if (playerController == null)
-                playerController = (GameObject.FindGameObjectWithTag("Player")).GetComponent<PlayerController>();
-        }
-        else if (currentLevel == Scenes.MainMenu)
-        {
-            MenuInstances[0] = newCanvas(Menus.Title);
-            if (PlayerPrefs.HasKey("lvl"))
+            MenuInstances.Clear();
+            for (int i = 0; i < MenuPrefabsDONOTTOUCH.Count + 1; i++)
+                MenuInstances.Add(null);
+            transform.parent = Camera.main.transform;
+            UpdateMusic(musicVol);
+            UpdateSFx(sfxVol);
+            pause = false;
+            music.pitch = 1.0f;
+            Time.timeScale = 1.0f;
+            if (currentLevel != Scenes.MainMenu)
             {
-                int hour = PlayerPrefs.GetInt("date_Hour");
-                string ampm = " AM";
-                if (hour > 12)
-                {
-                    hour -= 12;
-                    ampm = " PM";
-                }
-                string date = PlayerPrefs.GetInt("date_Month") + "/" + PlayerPrefs.GetInt("date_Day") + "/" + PlayerPrefs.GetInt("date_Year") + "\t" + hour + ":" + PlayerPrefs.GetInt("date_Minute") + ampm;
-                Button loadButton = GameObject.Find("LoadButton").GetComponent<Button>();
-                loadButton.interactable = true;
-                loadButton.GetComponentsInChildren<Text>()[1].text = date;
+                if (playerController == null)
+                    playerController = (GameObject.FindGameObjectWithTag("Player")).GetComponent<PlayerController>();
             }
-            activeMenu = Menus.Title;
+            else if (currentLevel == Scenes.MainMenu)
+            {
+                MenuInstances[0] = newCanvas(Menus.Title);
+                if (PlayerPrefs.HasKey("lvl"))
+                {
+                    int hour = PlayerPrefs.GetInt("date_Hour");
+                    string ampm = " AM";
+                    if (hour > 12)
+                    {
+                        hour -= 12;
+                        ampm = " PM";
+                    }
+                    string date = PlayerPrefs.GetInt("date_Month") + "/" + PlayerPrefs.GetInt("date_Day") + "/" + PlayerPrefs.GetInt("date_Year") + "\t" + hour + ":" + PlayerPrefs.GetInt("date_Minute") + ampm;
+                    Button loadButton = GameObject.Find("LoadButton").GetComponent<Button>();
+                    loadButton.interactable = true;
+                    loadButton.GetComponentsInChildren<Text>()[1].text = date;
+                }
+                activeMenu = Menus.Title;
+            }
         }
     }
 
     private void Update()
     {
-        if (currentLevel != Scenes.MainMenu)
+        if (currentLevel != Scenes.MainMenu && Application.loadedLevelName != "LoadingScreen")
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
@@ -311,6 +314,11 @@ public class GameManager : MonoBehaviour
         currentLevel = _scene;
         Time.timeScale = 1.0f;
         transform.parent = null;
+        Application.LoadLevel("LoadingScreen");
+    }
+
+    public void FinishLoading()
+    {
         Application.LoadLevel((int)currentLevel);
     }
 
