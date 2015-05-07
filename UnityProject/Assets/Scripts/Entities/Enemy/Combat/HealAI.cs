2463906820b579cc794f5944b69a7f1320f79fac
Devin Aldrich <19yoshi95@gmail.com>
2015-05-07 15:05:54 -0400

    Orc Chief updated!
    Files Edited:
    	-Orc Chief
    		-added the healing ability
    	-Assets/Sprites/Particles
    		-created folder
    	-HealParticle.png
    		-created for use as a particle when healing
    	-HealParticle.mat
    		-created for use as a particle when healing
    	-Assets/Prefabs/Components/Particle Emitters
    		-created to store particle effect objects
    	-HealthEffect
    		-created for use with HealAI
    	-HealAI
    		-created
    
    Signed-off-by: Devin Aldrich <19yoshi95@gmail.com>

---
+++ UnityProject/Assets/Scripts/Entities/Enemy/Combat/HealAI.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealAI : AttackAI {
+
+    private Health health;
+    private ParticleSystem psys;
+
+    public float healAmount;
+    public GameObject particleEmitter;
+
+	// Use this for initialization
+	protected override void Start () {
+        base.Start();
+
+        GameObject emitter = (GameObject)Instantiate(particleEmitter, transform.position, transform.rotation);
+        emitter.transform.parent = transform;
+        psys = emitter.GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+    protected override void Update()
+    {
+        if(health == null)
+            health = GetComponent<Health>();
+
+        bool notAggro = false;
+
+        if (coordinator.OnSightMovement != "Seek_Movement")
+        {
+            if (coordinator.nonseekAggroTimerMax <= 0.0f)
+                notAggro = true;
+        }
+        else
+        {
+            if (coordinator.currentMovement.GetType() != System.Type.GetType("Seek_Movement"))
+                notAggro = true;
+        }
+
+        if (notAggro)
+        {
+            if (reloadTimer > 0.0f)
+                reloadTimer -= Time.deltaTime;
+            else if (health.currentHP < health.maxHP)
+            {
+                health.Heal(healAmount);
+                psys.Emit((int)healAmount);
+                reloadTimer = reloadTimerMax;
+            }
+        }
+        else
+            reloadTimer = reloadTimerMax;
+    }
+}
