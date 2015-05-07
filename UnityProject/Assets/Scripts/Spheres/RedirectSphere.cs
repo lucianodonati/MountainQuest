dffedb02f78304697589847cc5786ce321de4898
Garrett <gldavis@fullsail.edu>
2015-05-07 16:29:14 -0400

    Moved images and animation stuff for spheres to proper folders. Started shield sphere

---
+++ UnityProject/Assets/Scripts/Spheres/RedirectSphere.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RedirectSphere : MonoBehaviour
+{
+    public float RotationDirection;
+    public Vector3 Direction = new Vector3(0, 1, 0);
+    public float DamageModifier = 5f;
+    public float AliveTimer = 7f;
+    public float PlayerRedirectMagnitude = 30f;
+    public float antigravityTime = 0.2f;
+    public Player Owner;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, RotationDirection));
+        if (AliveTimer != -1)
+        {
+            AliveTimer -= Time.deltaTime;
+            if (AliveTimer <= 0)
+            {
+                if (Owner != null)
+                    if (Owner.GetComponent<Player>())
+                        Owner.GetComponent<Player>().RemoveRSphere();
+                Destroy(this.gameObject);
+            }
+        }
+
+        if (AliveTimer <= 2)
+        {
+            SpriteRenderer mySR = GetComponent<SpriteRenderer>();
+            mySR.color = new Color(1, 1, 1, AliveTimer / 2);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Arrow>() != null)
+        {
+            SoundFX sfx = GetComponent<SoundFX>();
+            if (sfx != null)
+                sfx.Play("EnterSphere");
+
+            GameManager.instance.stats.arrowsRedirected++;
+            other.rigidbody2D.position = this.transform.position;
+            Direction.Normalize();
+            Direction *= other.rigidbody2D.velocity.magnitude;
+            other.rigidbody2D.velocity = Direction;
+            other.rigidbody2D.rotation = RotationDirection + 0;
+
+            other.GetComponent<Arrow>().damageType.damage += DamageModifier;
+        }
+        else if (other.tag == "Player")
+        {
+            SoundFX sfx = GetComponent<SoundFX>();
+            if (sfx != null)
+                sfx.Play("EnterSphere");
+
+            other.rigidbody2D.position = this.transform.position;
+            Direction.Normalize();
+            Direction *= PlayerRedirectMagnitude;
+            other.rigidbody2D.velocity = Direction;
+            other.GetComponent<PlayerController>().redirectedTimer = antigravityTime;
+            other.rigidbody2D.gravityScale = 0;
+        }
+    }
+
+    public void ChangeDirection(float rdir, Vector3 dir, float timer)
+    {
+        Direction = dir;
+        RotationDirection = rdir;
+        AliveTimer = timer;
+    }
+
+    public void SetOwner(Player owner)
+    {
+        Owner = owner;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (name == "JumpThroughRedirectSphere1" || name == "JumpThroughRedirectSphere2")
+            Destroy(gameObject);
+    }
+}
---
+++ UnityProject/Assets/Scripts/Spheres/ShieldSphere.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldSphere : MonoBehaviour {
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        
+    }
+}
