aec29621b75aeaa543e98b2a0b3521a88d49f7de
Garrett <gldavis@fullsail.edu>
2015-05-07 20:22:12 -0400

    Reorganized files. Shield sphere almost done.

--- UnityProject/Assets/Scripts/Spheres/BoostSphere.cs
+++ UnityProject/Assets/Scripts/Spheres/BoostSphere.cs
@@ -1,33 +1,10 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class BoostSphere : MonoBehaviour
+public class BoostSphere : BaseSphere
 {
     public float DamageModifier = 10;
     public float VelocityModifier = 1.5f;
-    public float AliveTimer = 7;
-    public Player Owner;
-
-    private void Update()
-    {
-        if (AliveTimer != -1)
-        {
-            AliveTimer -= Time.deltaTime;
-            if (AliveTimer <= 0)
-            {
-                if (Owner != null && Owner.GetComponent<Player>() != null)
-                    Owner.GetComponent<Player>().RemoveBSphere();
-                Destroy(this.gameObject);
-            }
-        }
-
-        if (AliveTimer <= 2)
-        {
-            SpriteRenderer mySR = GetComponent<SpriteRenderer>();
-
-            mySR.color = new Color(1, 1, 1, AliveTimer / 2);
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
--- UnityProject/Assets/Scripts/Spheres/ShieldSphere.cs
+++ UnityProject/Assets/Scripts/Spheres/ShieldSphere.cs
@@ -1,9 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShieldSphere : MonoBehaviour {
+public class ShieldSphere : BaseSphere {
+    public float magnitude;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Arrow proj = other.GetComponent<Arrow>();
+        if (proj != null)
+        {
+            magnitude = Vector3.Distance(transform.position, other.transform.position);
+
+            SoundFX sfx = GetComponent<SoundFX>();
+            if (sfx != null)
+                sfx.Play("EnterSphere");
+        }
+    }
     private void OnTriggerStay2D(Collider2D other)
     {
-        
+        Arrow proj = other.GetComponent<Arrow>();
+        if (proj != null)
+        {
+            if (other.rigidbody2D.velocity.magnitude < 32)
+            {
+                Vector3 pushVector = other.transform.position - transform.position;
+                other.rigidbody2D.velocity += new Vector2(pushVector.x, pushVector.y);
+                if (other.rigidbody2D.velocity.magnitude > 32)
+                    other.rigidbody2D.velocity = other.rigidbody2D.velocity.normalized * 32;
+            }
+        }
+    }
+
+    public void SetOwner(Player owner)
+    {
+        Owner = owner;
     }
 }
