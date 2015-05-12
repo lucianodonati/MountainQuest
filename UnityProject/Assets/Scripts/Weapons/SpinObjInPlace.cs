83317ee74f60469fe559a18ff125c7af6e8d5177
Garrett <gldavis@fullsail.edu>
2015-05-12 00:50:38 -0400

    Continue lightning arrow.

---
+++ UnityProject/Assets/Scripts/Weapons/SpinObjInPlace.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinObjInPlace : MonoBehaviour {
+    public float spinSpeed = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.AngleAxis(360 * spinSpeed * Time.deltaTime, new Vector3(0.0f, 0.0f, 1.0f));
+	}
+}
