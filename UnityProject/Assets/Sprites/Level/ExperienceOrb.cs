c5d67756216798b69497fa7c8a5cf06a202b14a0
Devin Aldrich <19yoshi95@gmail.com>
2015-05-16 20:33:49 -0400

    Level 4 updated to fix camera. Pull from here.
    Signed-off-by: Devin Aldrich <19yoshi95@gmail.com>

---
+++ UnityProject/Assets/Sprites/Level/ExperienceOrb.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExperienceOrb : MonoBehaviour {
+
+    private float changeTimer;
+    public float changeTimerMax;
+
+    private float Xrot, Yrot, Zrot;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (changeTimer > 0.0f)
+            changeTimer -= Time.deltaTime;
+        else
+        {
+            Xrot = Random.Range(0.0f, 5.0f);
+            Yrot = Random.Range(0.0f, 5.0f);
+            Zrot = Random.Range(0.0f, 5.0f);
+            changeTimer = changeTimerMax;
+        }
+
+        transform.Rotate(Xrot,Yrot,Zrot);
+	}
+}
