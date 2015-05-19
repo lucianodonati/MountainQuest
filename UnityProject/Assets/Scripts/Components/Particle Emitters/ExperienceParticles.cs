fb2cf526acea5650c36075ae01dbe8bb0e1ce3be
Luciano Donati <luciano.donati536@gmail.com>
2015-05-19 17:25:20 -0400

    Updated experience orb and fixed fire arrow particle effect

--- UnityProject/Assets/Scripts/Components/Particle Emitters/ExperienceParticles.cs
+++ UnityProject/Assets/Scripts/Components/Particle Emitters/ExperienceParticles.cs
@@ -1,12 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
 using UnityEditor;
-using System.Collections;
+using UnityEngine;
 
-public class ExperienceParticles : MonoBehaviour {
+public class ExperienceParticles : MonoBehaviour
+{
+    private ParticleSystem psys;
+    private ParticleSystem.Particle[] particles;
+    private int particlecount;
 
-    ParticleSystem psys;
-    ParticleSystem.Particle[] particles;
-    int particlecount;
     [HideInInspector]
     public int experience;
 
@@ -14,28 +15,32 @@ public class ExperienceParticles : MonoBehaviour {
     public float hangTimeMax = 1.0f;
     public float particleHangTimeSpeed = 1.0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    private void Start()
+    {
         psys = gameObject.AddComponent<ParticleSystem>();
         psys.simulationSpace = ParticleSystemSimulationSpace.World;
         psys.emissionRate = 0;
-        psys.startColor = new Color(205.0f/255.0f, 208.0f/255.0f, 8.0f/255.0f, 255/255.0f);
+        psys.startColor = new Color(205.0f / 255.0f, 208.0f / 255.0f, 8.0f / 255.0f, 255 / 255.0f);
         psys.startLifetime = 100.0f;
+
+        psys.startSize = 1;
         psys.Emit(experience);
 
         particles = new ParticleSystem.Particle[psys.maxParticles];
 
         hangTime = hangTimeMax;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
         particlecount = psys.GetParticles(particles);
 
         if (hangTime > 0.0f)
             hangTime -= Time.deltaTime;
 
-        for(int i = 0; i < particlecount; ++i)
+        for (int i = 0; i < particlecount; ++i)
         {
             if (hangTime > 0.0f)
                 particles[i].velocity = (particles[i].position - transform.position).normalized * (particleHangTimeSpeed * (hangTime / hangTimeMax));
@@ -49,5 +54,5 @@ public class ExperienceParticles : MonoBehaviour {
         }
 
         psys.SetParticles(particles, particlecount);
-	}
+    }
 }
